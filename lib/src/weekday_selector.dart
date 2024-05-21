c23b070ab57f58acb81c5ab7e49ca0c3c6a1f21e
Rodrigo Rafael <rodrigorafaeldamaceno@gmail.com>
2024-05-21 11:02:29 -0300

    flutter 3.22

--- lib/src/weekday_selector.dart
+++ lib/src/weekday_selector.dart
@@ -473,7 +473,7 @@ class WeekdayButton extends StatelessWidget {
           theme.buttonTheme.colorScheme!.onSurface.withOpacity(0.16);
       currentTextStyle = textStyle ??
           weekdayTheme?.textStyle ??
-          theme.textTheme.bodyText2!.copyWith(color: currentColor);
+          theme.textTheme.bodyMedium!.copyWith(color: currentColor);
       currentShape = shape;
     } else if (onPressed != null && selected == true) {
       currentColor = selectedColor ??
@@ -496,7 +496,7 @@ class WeekdayButton extends StatelessWidget {
       currentShape = selectedShape ?? weekdayTheme?.selectedShape;
       currentTextStyle = selectedTextStyle ??
           weekdayTheme?.selectedTextStyle ??
-          theme.textTheme.bodyText2!.copyWith(color: currentColor);
+          theme.textTheme.bodyMedium!.copyWith(color: currentColor);
     } else {
       currentDisabledElevation =
           disabledElevation ?? weekdayTheme?.disabledElevation ?? 0;
@@ -509,7 +509,7 @@ class WeekdayButton extends StatelessWidget {
       currentShape = disabledShape ?? weekdayTheme?.disabledShape;
       currentTextStyle = disabledTextStyle ??
           weekdayTheme?.disabledTextStyle ??
-          theme.textTheme.bodyText2!.copyWith(color: currentColor);
+          theme.textTheme.bodyMedium!.copyWith(color: currentColor);
     }
 
     return Expanded(
